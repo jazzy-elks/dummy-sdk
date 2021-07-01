acc5e93c3c041bfedb668f4a532d2d9c21cb7cfd
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-06-30 17:55:52 -0700

    added basic files

---
+++ config.js
@@ -0,0 +1,21 @@
+import EventSourceClient from './eventSourceClient';
+
+class Config {
+  constructor(serverAddress, sdkKey) {
+    this.serverAddress = serverAddress;
+    this.sdkKey = sdkKey;
+  }
+
+  connect() {
+    const eventSourceClient = new EventSourceClient(this);
+    this.eventSourceClient = eventSourceClient;
+    eventSourceClient.start();
+    return eventSourceClient;
+  }
+
+  getServerAddress() {
+    return `${this.serverAddress}/${this.sdkKey}`;
+  }
+}
+
+export default Config
---
+++ mocks/eventSourceClient.js
@@ -0,0 +1,54 @@
+import eventTypes from './lib/eventTypes';
+
+class EventSourceClient {
+  constructor(config) {
+    // todo: typechecks
+
+    this.config = config;
+    this.features = {};
+    
+    const apiClient = new EventSource(config.getServerAddress());
+    this.apiClient = apiClient;
+  }
+
+  start() {
+    this.handleEvents();
+    this.handleErrors();
+  }
+
+  handleEvents() {
+    this.apiClient.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      const eventType = data.eventType;
+      const payload = data.payload;
+      switch (eventType) {
+        case eventTypes.FEATURE_UPDATE:
+          this.handleFeatureUpdate(payload);
+        case eventTypes.FEATURE_UPDATES:
+          this.handleFeatureUpdates(payload);
+      }
+    }
+  }
+
+  handleErrors() {
+    this.apiClient.onerror = (error) => {
+      console.log("Event source failed: ", error);
+    }
+  }
+
+  handleFeatureUpdate(payload) {
+    const { key, value } = payload;
+    this.features[key] = value;
+  }
+
+  handleFeatureUpdates(payload) {
+    this.features = payload;
+  }
+
+  getFeature(key) {
+    const value = this.features[key];
+    return value;
+  }
+}
+
+export default EventSourceClient
